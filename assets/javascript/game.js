1d3d2f50f58aea8a62d9a728e8c7a39c6ab37595
sofyahoward <sofya@vt.edu>
2018-06-05 18:23:29 -0400

    complete word game with interactive background

---
+++ assets/javascript/game.js
@@ -0,0 +1,296 @@
+window.onload = function () {
+
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+          'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+          't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    
+    var categories;         // Array of topics
+    var chosenCategory;     // Selected catagory
+    var getHint ;          // Word getHint
+    var word ;              // Selected word
+    var guess ;             // Geuss
+    var geusses = [ ];      // Stored geusses
+    var lives ;             // Lives
+    var counter ;           // Count correct geusses
+    var space;              // Number of spaces in word '-'
+  
+    // Get elements
+    var showLives = document.getElementById("mylives");
+    var showCatagory = document.getElementById("scatagory");
+    var getHint = document.getElementById("hint");
+    var showClue = document.getElementById("clue");
+  
+    // create alphabet ul
+    var buttons = function () {
+      myButtons = document.getElementById('buttons');
+      letters = document.createElement('ul');
+  
+      for (var i = 0; i < alphabet.length; i++) {
+        letters.id = 'alphabet';
+        list = document.createElement('li');
+        list.id = 'letter';
+        list.innerHTML = alphabet[i];
+        check();
+        myButtons.appendChild(letters);
+        letters.appendChild(list);
+      }
+    }
+      
+    
+    // Select Catagory
+    var selectCat = function () {
+      if (chosenCategory === categories[0]) {
+        categoryName.innerHTML = "The Chosen Category Is Movies";
+      } else if (chosenCategory === categories[1]) {
+        categoryName.innerHTML = "The Chosen Category Is Sports";
+      } else if (chosenCategory === categories[2]) {
+        categoryName.innerHTML = "The Chosen Category Is Food";
+      }
+    }
+  
+    // Create geusses ul
+     result = function () {
+      wordHolder = document.getElementById('hold');
+      correct = document.createElement('ul');
+  
+      for (var i = 0; i < word.length; i++) {
+        correct.setAttribute('id', 'my-word');
+        guess = document.createElement('li');
+        guess.setAttribute('class', 'guess');
+        if (word[i] === "-") {
+          guess.innerHTML = "-";
+          space = 1;
+        } else {
+          guess.innerHTML = "_";
+        }
+  
+        geusses.push(guess);
+        wordHolder.appendChild(correct);
+        correct.appendChild(guess);
+      }
+    }
+    
+    // Show lives
+     comments = function () {
+      showLives.innerHTML = "You have " + lives + " lives";
+      if (lives < 1) {
+        showLives.innerHTML = "Game Over";
+      }
+      for (var i = 0; i < geusses.length; i++) {
+        if (counter + space === geusses.length) {
+          showLives.innerHTML = "You Win!";
+        }
+      }
+    }
+  
+   
+  
+    // OnClick Function
+     check = function () {
+      list.onclick = function () {
+        var geuss = (this.innerHTML);
+        this.setAttribute("class", "active");
+        this.onclick = null;
+        for (var i = 0; i < word.length; i++) {
+          if (word[i] === geuss) {
+            geusses[i].innerHTML = geuss;
+            counter += 1;
+          } 
+        }
+        var j = (word.indexOf(geuss));
+        if (j === -1) {
+          lives -= 1;
+          comments();
+        //   animate();
+        } else {
+          comments();
+        }
+      }
+    }
+    
+      
+    // Play
+    play = function () {
+      categories = [
+          ["avengers", "ant-man", "skyscraper", "incredibles", "smallfoot"],
+          ["tennis", "soccer", "golf", "basketball", "swimming"],
+          ["cake", "pasta", "pizza", "salad", "canoli"]
+      ];
+  
+      chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+      word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+      word = word.replace(/\s/g, "-");
+      console.log(word);
+      buttons();
+  
+      geusses = [ ];
+      lives = 10;
+      counter = 0;
+      space = 0;
+      result();
+      comments();
+      selectCat();
+      
+    }
+  
+    play();
+    
+    // Hint
+  
+      hint.onclick = function() {
+  
+        hints = [
+          ["A Popular Marvel Movie", "A Very Small Superhero", "A New Movie With The Rock", "A Disney Pixar Flick", "The Opposite of Bigfoot"],
+          ["Serena Plays That", "Messi...The Only Hint You Get", "Involves A Very Small White Ball", "Dunk It", "A Water Sport"],
+          ["Smash This On Your First Birthday", "Italian Staple", "Round, Cheesy, and Yummy", "Healthy Food You Got To Eat", "Sweet, Small and Italian"]
+      ];
+  
+      var catagoryIndex = categories.indexOf(chosenCategory);
+      var hintIndex = chosenCategory.indexOf(word);
+      showClue.innerHTML = "Clue: " +  hints [catagoryIndex][hintIndex];
+    };
+  
+     // Reset
+  
+    document.getElementById('reset').onclick = function() {
+      correct.parentNode.removeChild(correct);
+      letters.parentNode.removeChild(letters);
+      showClue.innerHTML = "";
+    //   context.clearRect(0, 0, 400, 400);
+      play();
+    }
+   
+   
+   
+    
+    
+function Particle( x, y, radius ) {
+	this.init( x, y, radius );
+}
+
+Particle.prototype = {
+
+	init: function( x, y, radius ) {
+
+		this.alive = true;
+
+		this.radius = radius || 10;
+		this.wander = 0.15;
+		this.theta = random( TWO_PI );
+		this.drag = 0.92;
+		this.color = '#fff';
+
+		this.x = x || 0.0;
+		this.y = y || 0.0;
+
+		this.vx = 0.0;
+		this.vy = 0.0;
+	},
+
+	move: function() {
+
+		this.x += this.vx;
+		this.y += this.vy;
+
+		this.vx *= this.drag;
+		this.vy *= this.drag;
+
+		this.theta += random( -0.5, 0.5 ) * this.wander;
+		this.vx += sin( this.theta ) * 0.1;
+		this.vy += cos( this.theta ) * 0.1;
+
+		this.radius *= 0.96;
+		this.alive = this.radius > 0.5;
+	},
+
+	draw: function( ctx ) {
+
+		ctx.beginPath();
+		ctx.arc( this.x, this.y, this.radius, 0, TWO_PI );
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	}
+};
+
+// ----------------------------------------
+// Example
+// ----------------------------------------
+
+var MAX_PARTICLES = 280;
+var COLOURS = [ '#69D2E7', '#A7DBD8', '#E0E4CC', '#F38630', '#FA6900', '#FF4E50', '#F9D423' ];
+
+var particles = [];
+var pool = [];
+
+var demo = Sketch.create({
+	container: document.getElementById( 'container' )
+});
+
+demo.setup = function() {
+
+	// Set off some initial particles.
+	var i, x, y;
+
+	for ( i = 0; i < 20; i++ ) {
+		x = ( demo.width * 0.5 ) + random( -100, 100 );
+		y = ( demo.height * 0.5 ) + random( -100, 100 );
+		demo.spawn( x, y );
+	}
+};
+
+demo.spawn = function( x, y ) {
+
+	if ( particles.length >= MAX_PARTICLES )
+		pool.push( particles.shift() );
+
+	particle = pool.length ? pool.pop() : new Particle();
+	particle.init( x, y, random( 5, 40 ) );
+
+	particle.wander = random( 0.5, 2.0 );
+	particle.color = random( COLOURS );
+	particle.drag = random( 0.9, 0.99 );
+
+	theta = random( TWO_PI );
+	force = random( 2, 8 );
+
+	particle.vx = sin( theta ) * force;
+	particle.vy = cos( theta ) * force;
+
+	particles.push( particle );
+}
+
+demo.update = function() {
+
+	var i, particle;
+
+	for ( i = particles.length - 1; i >= 0; i-- ) {
+
+		particle = particles[i];
+
+		if ( particle.alive ) particle.move();
+		else pool.push( particles.splice( i, 1 )[0] );
+	}
+};
+
+demo.draw = function() {
+
+	demo.globalCompositeOperation  = 'lighter';
+	
+	for ( var i = particles.length - 1; i >= 0; i-- ) {
+		particles[i].draw( demo );
+	}
+};
+
+demo.mousemove = function() {
+
+	var particle, theta, force, touch, max, i, j, n;
+
+	for ( i = 0, n = demo.touches.length; i < n; i++ ) {
+
+		touch = demo.touches[i], max = random( 1, 4 );
+		for ( j = 0; j < max; j++ ) demo.spawn( touch.x, touch.y );
+	}
+};
+
+
+}
